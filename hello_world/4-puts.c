7d6a374dba45ebb442cbade3ad65b6757d0671ee
Ethan1709 <benyayerethan@gmail.com>
2022-09-29 14:19:36 -0700

    c programm

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-int puts (const char *"Programming is like building  a multilingual puzzle )
+#include<stdio.h>
+
+/**
+ *main - this is a c code
+ *
+ *Description: use puts and displays a sentence
+ *Return: Return 0 to exit function
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle" );
+
+    return 0;
+    }
