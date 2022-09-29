806490dbc07cc457e3c08fd96357c397fce86cfa
Ethan1709 <benyayerethan@gmail.com>
2022-09-29 14:42:51 -0700

    printf c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include<stdio>
 
+/**
+ * main - run the code
+ **
+ * Description: use printf function
+ * Return: Return 0 to exit function 
+ *
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is piece of art"); 
