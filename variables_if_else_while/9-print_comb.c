ef754f7729fbbdb15c25bbd8d9ea135c78812bdf
Ethan1709 <benyayerethan@gmail.com>
2022-10-03 08:02:20 -0700

    print digits

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints digits
+ * Return: Alaways 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	putchar(ch);
+	if (ch == 9)
+		break;
+	putchar(',')
+	 putchar(' ');
+	putchar('\n');
+	return (0);
+}
