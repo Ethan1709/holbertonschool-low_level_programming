51ce652602479098c5a111d10bef946182f63c3c
Ethan1709 <benyayerethan@gmail.com>
2022-10-03 05:17:28 -0700

    upper and lowercase alphabet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,14 @@
 
 int main(void)
 {
-int ch;
+	int ch;
 
-for (ch = 'A'; ch <= 'Z'; ch++)
-putchar(ch);
-putchar ('\n');
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
 
-for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
-putchar ('\n');
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
 
-return (0);
+	putchar ('\n');
+	return (0);
 }
