e8e3052a131683ab480dfe8ab2840e302791bdd5
Ethan1709 <benyayerethan@gmail.com>
2022-10-03 08:09:17 -0700

    numbers

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,11 +11,12 @@ int main(void)
 	int ch;
 
 	for (ch = '0'; ch <= '9'; ch++)
-	putchar(ch);
-	if (ch == 9)
-		break;
-	putchar(',')
-	 putchar(' ');
-	putchar('\n');
+		putchar(ch);
+		if (ch == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+		putchar('\n');
+
 	return (0);
 }
