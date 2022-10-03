7c365d3bf4244c9eb74cc19f53a5b3eff26b0965
Ethan1709 <benyayerethan@gmail.com>
2022-10-03 07:21:06 -0700

    hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints digits
+ * Return: Alaways 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	for (ch = 'a'; ch <= 'F'; ch++)
+		putchar(ch)
+	putchar('\n');
+	return (0);
+}
