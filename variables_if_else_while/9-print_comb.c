34c8d2b8272337e61806960450894728cfba0c25
Ethan1709 <benyayerethan@gmail.com>
2022-10-03 08:15:39 -0700

    numberz

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,12 +11,12 @@ int main(void)
 	int ch;
 
 	for (ch = '0'; ch <= '9'; ch++)
-		putchar(ch);
+	{	putchar(ch);
 		if (ch == 9)
 			break;
 		putchar(',');
 		putchar(' ');
 		putchar('\n');
-
+	}
 	return (0);
 }
