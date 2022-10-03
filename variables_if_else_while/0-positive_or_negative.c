ca7476b76147cf51f127477b1add0c461c12280f
Ethan1709 <benyayerethan@gmail.com>
2022-10-03 01:35:26 -0700

    negative or pos

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,18 @@
-jdzjdsjdhjdskjdksjdkjdskdjkjk
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function maiin goes there */
+int main(void)
+{
+  int n;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  if (n > 0)
+    printf("%d is positive/n", n);
+  if (n = 0)
+    printf("%d is zero/n"), n);
+  if (n < 0)
+    printf("%d is negative/n", n);
+return (0);
+}
