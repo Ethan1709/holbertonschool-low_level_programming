661ec83ac6ef939b2bc336c4292a213419e8abb9
Ethan1709 <benyayerethan@gmail.com>
2022-10-04 03:09:00 -0700

    alphabet.h

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints _putchar
+ * Return: Alaways 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	for (char x = 'a'; x <= 'z'; x++)
+		putchar(x);
+			putchar('\n');
+}
