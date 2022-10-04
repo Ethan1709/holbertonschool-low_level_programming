ce662678236e37cf7463238118ec2f32ea4ace2c
Ethan1709 <benyayerethan@gmail.com>
2022-10-04 02:24:25 -0700

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ * Description: prints _putchar
+ * Return: Alaways 0 (Success)
+ */
+
+int main(void)
+{
+	putchar("_putchar\n");
+		return (0);
+}
