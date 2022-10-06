42f8c2a84bd93de5f33d0eeab7bb96d85b6df300
Ethan1709 <benyayerethan@gmail.com>
2022-10-06 01:51:00 -0700

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * Description: checks if character is in lowercase
+ *@c: _isalpha
+ * Return: Alaways 0 (Success)
+ */
+
+int _isalpha(int c)
+
+{
+	if (islapha(c))
+		return (1);
+	else
+		return (0);
+}
