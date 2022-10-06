d2fd8c26b62a0cbbcf69b8911cee06934ff270a2
Ethan1709 <benyayerethan@gmail.com>
2022-10-06 03:34:11 -0700

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description: checks if character is in lowercase
+ *@c: last digit
+ * Return: Alaways 0 (Success)
+ */
+
+int print_last_digit(int c)
+{
+	if (c >= 0)
+		return (c % 10);
+	else if (c == 0)
+		return (c % 10);
+	else 
+		return (c % 10);
+}
