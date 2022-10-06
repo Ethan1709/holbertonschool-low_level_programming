a029edf6fed845e77037f67871aa6e1fc83f0e0c
Ethan1709 <benyayerethan@gmail.com>
2022-10-06 02:40:12 -0700

    print sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - Entry point
+ * print_sign - Entry point
  * Description: checks if character is in lowercase
  *@n: sign
  * Return: Alaways 0 (Success)
@@ -11,15 +11,18 @@ int print_sign(int n)
 
 {
 	if (n > 0)
-	{	_putchar('+');
+	{
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
-	{	_putchar('0');
+	{
+		_putchar('0');
 		return (0);
 	}
-	else (n < 0)
-	{	_putchar('-');
+	else
+	{
+		_putchar('-');
 		return (-1);
 	}
 }
