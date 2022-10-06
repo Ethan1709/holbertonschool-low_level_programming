ad12d6934751b47292b9e191ffb704fd181a3817
Ethan1709 <benyayerethan@gmail.com>
2022-10-06 05:11:43 -0700

    every minute

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * void jack_bauer - Entry point
+ * Description: prints every minute
+ * Return: Alaways 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int h, m; 
+	{
+	for (h == 0; h <= 23; h++)
+	{	for (m == 0;  <=59; m++)
+			putchar((m / 10) + '0');
+			putchar(m % 10) + '0');
+	}	 putchar(:);
+		putchar(h / 10) + '0');
+		putchar(h % 10) + '0');
+	}
+	return (0);	
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,4 +6,6 @@ int _isalpha(int);
 int print_sign(int n);
 int _abs(int c);
 int print_last_digit(int c);
+void jack_bauer(void);
+
 
