3025ca9f28dd00d1a5de1099b6986982ce1a86fd
Ethan1709 <benyayerethan@gmail.com>
2022-10-06 07:31:24 -0700

    n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ * Description: n to 98
+ * @n: number
+ * Return: Alaways 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	int n;
+
+	while (n >= 1)
+	{
+		printf("%d\n", n);
+		n--;
+	}
+}
