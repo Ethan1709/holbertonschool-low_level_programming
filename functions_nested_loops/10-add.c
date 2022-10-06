f4716beefc15b4b11425e18d787830290e2df445
Ethan1709 <benyayerethan@gmail.com>
2022-10-06 07:20:27 -0700

    adds two ints

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description: adds two integers
+ * @x: first integer
+ * @y: first integer
+ * Return: Alaways 0 (Success)
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
