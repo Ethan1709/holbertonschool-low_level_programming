85edd6096945e68a5035f4694f395e307b7542ab
Ethan1709 <benyayerethan@gmail.com>
2022-10-06 03:02:26 -0700

    absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description: checks if character is in lowercase
+ *@int: absolute value
+ * Return: Alaways 0 (Success)
+ */
+
+int _abs(int)
+
+{
+	int c;
+
+	if (c > 0)
+		return (n);
+	else
+		return (-c);
+}
