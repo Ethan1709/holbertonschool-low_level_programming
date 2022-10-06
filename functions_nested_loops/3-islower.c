8d91ea405b6143956af1c255a3c5dbf7511381ca
Ethan1709 <benyayerethan@gmail.com>
2022-10-06 01:24:46 -0700

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints _putchar
+ * Return: Alaways 0 (Success)
+ */
+
+int _islower(int c)
+{
+	char ch;
+
+	if (islower(ch == c))
+		returns (1);
+	else (islower(ch != c))
+		returns (0);
+}
