8d0220e7e8ac9051a32d29d8b7f0d6ada2a7b38d
Ethan1709 <benyayerethan@gmail.com>
2022-10-07 00:40:59 -0700

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * _isupper - Entry point
+ * Description: checks for uppercase character
+ * @c: c is the character
+ * Return: return 1 or 0
+ */
+
+int _isupper(int c)
+
+{
+	if (_isupper(c))
+		return (1);
+
+	else
+		return (0);
+}
+
