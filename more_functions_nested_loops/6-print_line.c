a1ea7f4fa17005ac76ce469028c4b5964b2d5129
Ethan1709 <benyayerethan@gmail.com>
2022-10-07 02:55:32 -0700

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * Description: prints a straight line
+ * Return: Alaways 0 (Success)
+ */
+
+void print_line(int n)
+
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l > 0; l < n; l++)
+			_putchar('_');
+	}
+	_putchar('\n');
+
+
+
+}
