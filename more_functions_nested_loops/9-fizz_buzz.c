b1934462edf0c1cb8cde361eca094a12725219c8
Ethan1709 <benyayerethan@gmail.com>
2022-10-07 05:27:24 -0700

    Fizz Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints numbers from 0 to 14 ten times
+ * Return: Alaways 0 (Success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			putchar('Fizz');
+
+		else if (i % 5 == 0)
+			putchar('Buzz');
+
+		else if (i % 3 && i % 5 == 0)
+			putchar('Fizz Buzz');
+
+		else
+
+		putchar(' ');
+		putchar('\n');
+
+
+	}
+}
+
