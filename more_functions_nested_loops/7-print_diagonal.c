0c62a93a3812edc9d4e5f5624336658b3d8a031e
Ethan1709 <benyayerethan@gmail.com>
2022-10-07 03:00:29 -0700

    print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * Description: prints a straight line
+ * @n: n is the character
+ * Return: Alaways 0 (Success)
+ */
+
+void print_diagonal(int n)
+
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+			_putchar('\\');
+	}
+	_putchar('\n');
+
+
+
+}
