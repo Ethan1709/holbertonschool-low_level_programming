7ec909726c015b4acf45411a2b9d7dac2fa97c73
Ethan1709 <benyayerethan@gmail.com>
2022-10-07 02:16:26 -0700

    0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ * Description: prints numbers from 0 to 14 ten times
+ * Return: Alaways 0 (Success)
+ */
+
+void more_numbers(void)
+
+{
+	int n, i;
+
+	for (n = 0; n <= 9; n++)
+
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+		}
+
+		_putchar('\n');
+}
