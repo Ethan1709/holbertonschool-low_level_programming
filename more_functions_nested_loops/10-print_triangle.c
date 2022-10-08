aac639fe6d5b787ff744da3ec4fe176b2abd0433
Ethan1709 <benyayerethan@gmail.com>
2022-10-08 12:40:11 -0700

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -21,15 +21,17 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < i; j++)
-				_putchar('#');
-			_putchar(' ');
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ')
+			}
 
-			if (i == j - 1)
-				continue;
-			_putchar('\n');
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+
+			}
 		}
-	}
 	_putchar('\n');
 
 }
