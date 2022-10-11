6b60dafe628d1e21e8893b41864c26e2abc7c992
Ethan1709 <benyayerethan@gmail.com>
2022-10-11 06:58:13 -0700

    rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,26 @@
 #include "main.h"
 
 /**
- * print_rev - Entry point
+ * print_rev_logic - Entry point
  * Description: print string in rev
  * @s: is the first integer
  * Return: Alaways 0 (Success)
  */
+void print_rev_logic(char *s)
+{
+	if (!*s)
+		return;
+	print_rev_logic(s + 1);
+	_putchar(*s);
+}
 
+/**
+ * print_rev - function
+ * @s: char ptr
+ */
 void print_rev(char *s)
-
-	{
-		while (*s)
-			_putchar(*s--);
-
-		_putchar('\n');
-	}
-
+{
+	print_rev_logic(s);
+	return ((void) _putchar(10));
+}
 
