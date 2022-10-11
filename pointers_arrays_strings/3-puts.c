58565fca38c773bfeda84b9fae55c4a2fc9f9cfe
Ethan1709 <benyayerethan@gmail.com>
2022-10-11 06:15:05 -0700

    print string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
 /**
  * _puts - Entry point
  * Description: return the length of a string
- * @s: is the first integer
+ * @str: is the first integer
  * Return: Alaways 0 (Success)
  */
 
@@ -12,7 +12,7 @@ void _puts(char *str)
 	{
 		while (*str)
 			_putchar(*str++);
-	
+
 		_putchar('\n');
 	}
 
