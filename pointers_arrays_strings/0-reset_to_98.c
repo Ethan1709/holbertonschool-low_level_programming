04d4fe6e73493e4b37bba8fcd8f7000641fdff29
Ethan1709 <benyayerethan@gmail.com>
2022-10-11 00:55:42 -0700

    pointers to 8

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * Description: takes a pointer to a value of 98
+ *@n: n is the integer
+ * Return: Alaways 0 (Success)
+ */
+
+void reset_to_98(int *n);
+
+{
+	int n = 98;
+	int *ip;
+
+	ip = &n
+		return (0);
+}
+
+
