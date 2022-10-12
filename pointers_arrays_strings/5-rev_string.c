3a7926249763470b383d5fca84f7bf10cf88bc4e
Ethan1709 <benyayerethan@gmail.com>
2022-10-12 03:11:34 -0700

    rev sting

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,18 @@
  */
 void rev_string(char *s)
 {
-	int len = 1;
+	int len = 0;
 	int middle = len / 2;
 	char temp;
 	int i;
-
-	for (i = 0; i < middle; i++)
+	
+	while (len != '\0')
 	{
-		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		for (i = 0; i < middle; i++)
+		{
+			temp = s[i];
+			s[i] = s[len - i - 1];
+			s[len - i - 1] = temp;
 	}
 }
 
