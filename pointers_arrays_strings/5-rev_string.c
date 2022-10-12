f1dbef68eac4a59e8c950d48e2ce473a09354225
Ethan1709 <benyayerethan@gmail.com>
2022-10-12 02:39:24 -0700

    rev string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,14 @@
  */
 void rev_string(char *s)
 {
-	int length = strlen(s);
-	int middle = length / 2;
+	int len;
+	int middle = len / 2;
 	char temp;
 
 	for (i = 0; i < middle; i++)
 	{
 		temp = s[i];
-		s[i] = s[length - i - 1];
+		s[i] = s[len - i - 1];
 		s[length - i - 1] = temp;
 	}
 }
