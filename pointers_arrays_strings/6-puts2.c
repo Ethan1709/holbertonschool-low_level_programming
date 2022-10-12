f6674a704af191045c8528f133004308d3960e21
Ethan1709 <benyayerethan@gmail.com>
2022-10-12 03:34:27 -0700

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: is the string
+ * Return: Alaways 0 (Success)
+ */
+
+void puts2(char *str)
+
+	{
+		while (*str)
+			_putchar(*str += 2);
+
+		_putchar('\n');
+	}
+
+
