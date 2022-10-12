39ce9194fde22ea53d8e6a070c60a1ecb2a21eb7
Ethan1709 <benyayerethan@gmail.com>
2022-10-12 04:42:33 -0700

    print array

---
+++ pointers_arrays_strings/8-print_array.cs
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ * @str: is the string
+ * @n: integer
+ * Return: Alaways 0 (Success)
+ */
+
+void print_array(int a*, int n)
+
+	{
+		int i;
+
+		for (i = 0; i < n; i++);
+		{	
+			printf("%d", a[i]);
+
+			if ( i == n - 1)
+				continue;
+			printf(", ";
+		}
+
+		
+
+		_putchar('\n');
+	}
+
+
