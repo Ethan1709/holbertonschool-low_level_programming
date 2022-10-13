5cce783bd32711c30adf5cabcd5e493bd9483022
Ethan1709 <benyayerethan@gmail.com>
2022-10-13 03:50:48 -0700

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ * @src: is the string
+ * @dest: is the string
+ * @n: integer
+ * Return: Alaways 0 (Success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+	{
+		int i = 0;
+		int j = 0;
+
+		{
+			while (dest[j])
+				j++;
+
+			while (i < n && src[i])
+			{
+				dest[j] = (src[i]);
+					i++,
+			}
+		}
+		return (dest);
+	}
+
+
