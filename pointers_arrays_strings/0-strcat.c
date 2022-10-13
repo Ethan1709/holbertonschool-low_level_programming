820ae1d26ec5c5e126324d1aabdbfb6942196595
Ethan1709 <benyayerethan@gmail.com>
2022-10-13 01:08:21 -0700

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,12 @@ char *_strcat(char *dest, char *src)
 	{
 		int i = 0;
 		int j = 0;
-
+		int x = 0;
+		
 		while (src[i])
 		{
-		dest[j] = (src[i] + dest[j]);
-			i++;
+		dest[x] = (src[i] + dest[j]);
+			i++, j++;
 		}
 
 		return (dest);
