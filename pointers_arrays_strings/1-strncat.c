3d14fb5332730b595f72e06d9c4b2f77a7c142c4
Ethan1709 <benyayerethan@gmail.com>
2022-10-13 01:30:13 -0700

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - Entry point
+ * @src: is the string
+ * @dest: is the string
+ *@n: integer
+ * Return: Alaways 0 (Success)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+	{
+		int i = 0;
+		int j = 0;
+
+		while (dest[j])
+			j++;
+
+		while (src[i] =! n)
+		{
+		dest[j] = (src[i] + dest[j]);
+			i++, j++;
+		}
+
+		return (dest);
+	}
+
+
