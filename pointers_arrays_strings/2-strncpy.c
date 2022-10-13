adcba7808d92ba145cbeb77aca1294eca5c7668c
Ethan1709 <benyayerethan@gmail.com>
2022-10-13 04:32:42 -0700

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,15 @@
 char *_strncpy(char *dest, char *src, int n)
 
 	{
-		int i = 0;
+		int i;
 
-			for (i < n && src[i])
+			for (i = 0; i < n && src[i]; i++)
 			{
 				dest[i] = src[i];
-					i++;
 			}
 			for ( ; i < n; i++)
+				dest[i] = '\0';
+	
 		return (dest);
 	}
 
