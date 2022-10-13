2867c9647826615ee7869f70ca8da379a7f4ce43
Ethan1709 <benyayerethan@gmail.com>
2022-10-13 05:46:16 -0700

    rev

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ * Description: return the length of a string
+ * @str: is the first integer
+ * Return: Alaways 0 (Success)
+ */
+
+void reverse_array(int *a, int n)
+
+	{
+		while (a < n)
+			_putchar(*a++);
+
+		_putchar('\n');
+	}
+
+
