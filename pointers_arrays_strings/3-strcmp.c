a32916566d7e33f1c408fb7d1238a986f13d812a
Ethan1709 <benyayerethan@gmail.com>
2022-10-13 04:48:00 -0700

    cmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Entry point
+ * @s1: is the string
+ * @s2: is the string
+ * Return: Alaways 0 (Success)
+ */
+
+char *_strcmp(char *s1, char *s2)
+
+	{
+
+		while (*s1 && *s2 && *s1 == *s2)
+
+		*s1++;
+		*s2++;
+
+		return (*s1 - *s2);
+	}
+
+
