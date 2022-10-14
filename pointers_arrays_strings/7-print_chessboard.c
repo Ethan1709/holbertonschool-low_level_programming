b9bc679d808cb3c76e42796152e77aabdc56f5ec
Ethan1709 <benyayerethan@gmail.com>
2022-10-14 07:12:54 -0700

    chess

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - locates first occurence in a string
+ * @haystack: first string
+ * @needle: second string
+ *
+ * Return: Pointer to memory area
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(i, j);
+	}
+}
