8a7d931b37aac932c3ff40d403b3a2c3301dad60
Ethan1709 <benyayerethan@gmail.com>
2022-10-17 07:03:40 -0700

    power recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints factorial of a given number
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: Always success (0)
+ */
+
+int _pow_recursion(int x, int y);
+{
+	if (n x == 0)
+		return (0);
+	else 
+		return (_pow_recursion(x ** y);
+}
+
