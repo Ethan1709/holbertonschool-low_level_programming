460922fa025f226e8d5e161f58a10b0d677bc9b1
Ethan1709 <benyayerethan@gmail.com>
2022-10-17 03:52:35 -0700

    rev string

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string
+ * _print_rev_recursion - prints a string in reverse
  * @s: pointer
  *
  *
  * Return: Always success (0)
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
