9ce668ca34b90da4a9182782b8f074ff08c9dacb
Ethan1709 <benyayerethan@gmail.com>
2022-10-17 03:24:46 -0700

    prints a string

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -13,18 +13,7 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		s++;
+		++s;
 	}
-}
-
-int main()
-{
-	int s;
-
-	if (s = 0 && s != '\0')
-		_putchar(s);
-		s++;
-
-	return (0);
 }
 
