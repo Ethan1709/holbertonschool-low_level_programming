229137c743a5275547c609f7c9b3ab67a2cb6cbf
Ethan1709 <benyayerethan@gmail.com>
2022-10-17 05:08:04 -0700

    rev string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * strlen_recursion - prints a string in reverse
+ * @s: pointer
+ *
+ *
+ * Return: Always success (0)
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s++);
+		_putchar([s]);
+	}
+
+}
+
