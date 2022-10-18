16b66e9f0a9596dc4a2782158dde5f2190fbc074
Ethan1709 <benyayerethan@gmail.com>
2022-10-18 00:39:19 -0700

    prime number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * power - second fonction
+ * @x: first integer
+ * @y: second integer
+ * Return: Always success (0)
+ */
+
+int power(int x, int y)
+{
+	if (x * x == y)
+		return (0);
+
+	else if (x * x > y)
+		return (0);
+
+	else if (x * x < y)
+		return (0);
+
+	else
+		return (1);
+}
+
+/**
+ * _sqrt_recursion - main fonction
+ * @n: integer
+ *
+ *Return: Always success
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	else
+		return (power);
+}
+
+
