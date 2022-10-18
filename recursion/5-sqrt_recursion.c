9baf64afdaf8b66752f82ec528c294b3fbb5c8ec
Ethan1709 <benyayerethan@gmail.com>
2022-10-18 00:23:56 -0700

    root square

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * power - second fonction
+ * @x: first integer
+ * @y: second integer
+ * Return: Always success (0)
+ */
+
+int power(int x, y)
+{
+	if (x * x == y)
+		return (x);
+
+	else if (x * x > y)
+		return (-1);
+
+	else
+		return (power(x + 1, y));
+}
+
+/**
+ * _sqrt_recursion - main fonction
+ * @n: integer
+ *
+ *Return: Always success
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	else
+		return (power(1, n));
+}
+
+
