33734877da76fae13201cd463afd6b105cf77901
Ethan1709 <benyayerethan@gmail.com>
2022-10-19 03:57:13 -0700

    print all arg

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -7,10 +7,10 @@
  * Return: Success
  */
 
-int main(__attribute__ ((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", *argv);
-	argv++;
+	argc++;
 
 	return (0);
 }
