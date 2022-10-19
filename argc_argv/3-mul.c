aa60a89eada5bc40a007781bbaa3b27ed7cfc766
Ethan1709 <benyayerethan@gmail.com>
2022-10-19 04:22:19 -0700

    multiplies

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: command line
+ * @argv: char
+ * Return: Success
+ */
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	int i;
+	int j;
+	if (argc == '\0')
+		printf("Error")
+			return (1);
+	else
+		return (i * j);
+}
