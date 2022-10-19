0028c4b562b84c79a8b6db7c1677f9c6c43389fe
Ethan1709 <benyayerethan@gmail.com>
2022-10-19 03:19:55 -0700

    numb arg

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: command line
+ * @argv: char
+ * Return: Success
+ */
+
+int main(int argc, __attribute__((unused))  char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
