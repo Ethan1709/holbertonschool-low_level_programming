ba179d0b4b36f02e411b1b33112a687d3e394c71
Ethan1709 <benyayerethan@gmail.com>
2022-10-19 03:29:06 -0700

    print all arg

---
+++ argc_argv/2-argc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: command line
+ * @argv: char
+ * Return: Success
+ */
+
+int main(int argc, __attribute__((unused))  char *argv[])
+{
+	printf("%s\n", argc);
+	return (0);
+}
