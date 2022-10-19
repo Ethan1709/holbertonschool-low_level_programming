b42e96cd8d24edab6f7bae6f5410c9c7e3fbe43f
Ethan1709 <benyayerethan@gmail.com>
2022-10-19 02:42:59 -0700

    prints the name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-printf("argc = %d\n", argc)
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: command line
+ * @argv: char
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
