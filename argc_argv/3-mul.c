dc62aaa5f963be1298303a25fee0f23a9cf7dbc1
Ethan1709 <benyayerethan@gmail.com>
2022-10-19 06:52:22 -0700

    mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,9 +11,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
+	for (i = 1 && i = 2)
 	{
-		printf("%s\n", (argv[1] * argv[2]));
+		printf("%s\n", (argv[i]);
 	}
 	return (0);
 }
