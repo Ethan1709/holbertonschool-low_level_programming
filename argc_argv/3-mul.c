bed04fb923e06f04b5272c96b164f7c82b992022
Ethan1709 <benyayerethan@gmail.com>
2022-10-19 07:53:17 -0700

    muktpl

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -12,7 +13,6 @@ int main(int argc, char *argv[])
 	int i;
 	int j;
 	int result;
-	int atoi(const char *argv);
 
 	if (argc != 3)
 	{
