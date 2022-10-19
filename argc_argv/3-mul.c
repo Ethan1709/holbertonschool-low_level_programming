b777565c7ea1631934fe9b2dac9bb3727df7b0b1
Ethan1709 <benyayerethan@gmail.com>
2022-10-19 07:26:29 -0700

    muktpl

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,19 +9,17 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int j;
-	int result;
+	int i, j, result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-			return (1);
+		return (1);
 	}
 
-	i = atoi (argv[1]);
-	j = atoi (argv[2]);
-	result = i * j;
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	result = num1 * num2;
 
 	printf("%d\n", result);
 
