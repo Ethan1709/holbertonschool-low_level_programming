c213afe38ec5e24bf1127413129235b1099cdde8
Ethan1709 <benyayerethan@gmail.com>
2022-10-20 01:46:46 -0700

    sum

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 		for (j = 0; argv[i][j]; j++)
 		{
 			if (argv[i][j] < '0' || argv[i][j] > '9')
-				printf("Error\n");
+				printf("Error");
 		}
 		sum += atoi(argv[i]);
 	}
