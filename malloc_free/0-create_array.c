65c3a5fe751738700bb954de4c101a0e5737d5e6
Ethan1709 <benyayerethan@gmail.com>
2022-10-24 02:30:27 -0700

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,13 +3,26 @@
 #include <stdlib.h>
 
 /**
- *
+ * create_array - Entry point
+ * @size: int
+ * @c: char
+ * Return: char
  */
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return NULL;
+	unsigned int i;
+	char *p;
 
-	int * size = malloc(sizeof(c));
+	if (size == 0)
+		return (NULL);
+
+	else if (p == NULL)
+		return (NULL);
+
+	p = malloc(size * sizeof(c));
+
+	for (i = 0; i < size; i++)
+		p[i] = c;
+	return (p);
 }
