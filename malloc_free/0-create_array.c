183b942356f8c38415c475c9d2778d1a62c8a49c
Ethan1709 <benyayerethan@gmail.com>
2022-10-24 01:26:04 -0700

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return NULL;
 
-	int * size = malloc(sizeof(char c));
+	unsigned int * size = malloc(sizeof(char c));
 		_putchar('c');
 
 	return (0);
