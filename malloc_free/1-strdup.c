a297dbd5551ee150381be8a4af8a35c6f4c368a8
Ethan1709 <benyayerethan@gmail.com>
2022-10-24 04:23:26 -0700

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Entry point
+ * @size: int
+ * @c: char
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+	*p;
+	unsigned int i;
+
+	p = malloc(sizeof(str));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < str; i++)
+		p[i] = str;
+	return (p);
+}
