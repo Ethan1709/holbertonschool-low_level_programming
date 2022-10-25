092cd33934b3c5222c98c04b8c8248fb2673a91c
Ethan1709 <benyayerethan@gmail.com>
2022-10-25 00:27:44 -0700

    task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - return a pointer to a 2 dim arrays
+ * @width: int
+ * @height: int
+ * Return: always success
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int *p;
+	int i;
+	int j;
+
+	if (width <= 0)
+		return (NULL);
+
+	if (height <= 0)
+		return (NULL);
+
+	for (i = 0; i <= width; i++)
+		_putchar(i);
+
+	for (j = 0; j <= height; j++)
+		_putchar(j);
+
+	return (0);	
+}
