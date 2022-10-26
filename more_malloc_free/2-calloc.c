b0fcf14bc46e7efb09c617a5b30197a4cbff48d9
Ethan1709 <benyayerethan@gmail.com>
2022-10-26 06:07:18 -0700

    task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,19 +14,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *p;
 
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+
 	p = malloc(nmemb * size);
 	if (p != NULL)
 		memset(p, 0, nmemb * size);
 
-	if (nmemb == 0)
+	if (p == NULL)
 		return (NULL);
 
-	else if (size == 0)
-		return (NULL);
-
-	else if (p == NULL)
-		return (NULL);
-	
 	else
 		return (p);
 }
