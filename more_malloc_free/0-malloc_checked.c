46fe295038bcf604556123c7af7d19d66aea7f3d
Ethan1709 <benyayerethan@gmail.com>
2022-10-26 03:04:38 -0700

    task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -11,11 +11,13 @@
 void *malloc_checked(unsigned int b)
 {
 	int *p;
+	int i;
 
-	p = (int *) malloc(sizeof(b));
+	p = malloc(sizeof(b));
 
 	if (p == NULL)
 		return (NULL);
-	else
-		return (p);
+	for (i = 0; i < b; i++)
+		p[i] = b;
+	return (p);
 }
