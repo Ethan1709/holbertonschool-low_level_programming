9860d633614df3cf01f01be51001b04602a9d90e
Ethan1709 <benyayerethan@gmail.com>
2022-10-26 04:37:02 -0700

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			return (NULL);
 		p[k] = s1[k];
 	}
-	for (k = 0; s2[k] && k < n; k++)
+	for (k = 0; k <= n; k++)
 	{
 		if (p == NULL)
 			return (NULL);
