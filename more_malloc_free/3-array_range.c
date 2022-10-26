59e2563940681fb7c09f962b724300af3afbf4dd
Ethan1709 <benyayerethan@gmail.com>
2022-10-26 08:32:38 -0700

    task 3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -23,8 +23,8 @@ int *array_range(int min, int max)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = min; i <= max; i++)
+	for (i = min; i < max; i++)
 		p[i] = i;
 
-	return (p);
+	return (p[i]);
 }
