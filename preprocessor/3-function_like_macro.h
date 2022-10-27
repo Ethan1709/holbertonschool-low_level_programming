ed289a9bd7d23a2b34c3f7225792f3c8df77a498
Ethan1709 <benyayerethan@gmail.com>
2022-10-27 04:15:51 -0700

    task 3

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,17 +1,1 @@
-#include <stdio.h>
-
-/**
- * main - absolute value of x
- * Return: Success
- */
-
-int main(void)
-{ 
-	int x;
-
-	if (x > 0)
-		return (x);
-
-	else 
-		return (x * (-1));
-}
+#define abs(x)
