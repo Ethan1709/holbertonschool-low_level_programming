2de63c77e002a5e46928a5643b994cf96765dca8
Ethan1709 <benyayerethan@gmail.com>
2022-10-27 02:13:28 -0700

    task 2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program name
+ * @argc: name of the program
+ * @argv: number of elements
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("program's name is: %s\n",argv[0]);
+		return 0;
+}
