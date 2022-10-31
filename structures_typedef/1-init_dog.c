c77d3d1d5dcb0900fd7acabd30eb0d1605e86d46
Ethan1709 <benyayerethan@gmail.com>
2022-10-31 02:14:31 -0700

    task1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,27 @@
+#ifndef protection
+#define protection
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - dog caracteristics
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog' owner
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog dog ;
+
+	return (0);
+}
