bfba04e7c51dbd7c7f7b28a6b5186e5fe5236ce7
Ethan1709 <benyayerethan@gmail.com>
2022-10-31 05:39:44 -0700

    task2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - dog caracteristics
+ * @d: the pointer
+ */
+
+void print_dog(struct dog *d);
+{
+	if (d->name == NULL)
+		printf("Name: nil");
+	if (d->owner == NULL)
+		printf("Owner: nil");
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %d\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
