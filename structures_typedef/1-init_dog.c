f2743d08ad638965509f354221d1be3631ad2d49
Ethan1709 <benyayerethan@gmail.com>
2022-10-31 04:03:12 -0700

    task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -4,13 +4,14 @@
 
 /**
  * init_dog - dog caracteristics
+ * @d: the pointer
  * @name: name of the dog
  * @age: age of the dog
  * @owner: dog' owner
  */
 
- void init_dog(struct dog *d, char *name, float age, char *owner)
-{	
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
