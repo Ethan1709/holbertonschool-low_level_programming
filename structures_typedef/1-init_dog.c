0189c2710b0a7f4a2647e19f81ba5d65ca55c88a
Ethan1709 <benyayerethan@gmail.com>
2022-10-31 03:56:00 -0700

    task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,14 +10,6 @@
  * @age: age of the dog
  * @owner: dog' owner
  */
-
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
 #endif
 
  void init_dog(struct dog *d, char *name, float age, char *owner)
