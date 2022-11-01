96c5ee45729d3e03f19a320dacff2e1d8d90d480
Ethan1709 <benyayerethan@gmail.com>
2022-11-01 05:20:53 -0700

    task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - dog caracteristics
+ * @d: ptr
+ * Return: success
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == 0)
+		return (0);
+	else 
+		free(dog_t);
+}
+
