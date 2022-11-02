596b3471fcb7eec55d6e67ca606b6b8d4a4265e2
Ethan1709 <benyayerethan@gmail.com>
2022-11-02 07:10:21 -0700

    task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -20,9 +20,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if ((array)[i] = 98)
+		if (cmp(array[i]) != 0)
 			return (i);
 	}
-	return (-1);
+	return (0);
 }
 
