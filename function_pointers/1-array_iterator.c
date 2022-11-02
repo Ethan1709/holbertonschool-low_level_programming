504f5020d2593bf5ad67ba444b3c5b0cbffa0a05
Ethan1709 <benyayerethan@gmail.com>
2022-11-02 02:37:51 -0700

    task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - main function
+ * @array: int
+ * @size: the size
+ * @action: pointer
+ * Return: Success
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	action(array, size);
+}
