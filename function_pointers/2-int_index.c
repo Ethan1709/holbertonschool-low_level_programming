a2e934a3d0b75f3f1f6309152d1794595950e373
Ethan1709 <benyayerethan@gmail.com>
2022-11-02 04:14:38 -0700

    task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - main function
+ * @array: array
+ * @size: the size
+ * @cmp: pointer
+ * Return: Success
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= -1)
+		return (-1);
+
+	if (array == 0 || size == 0, || cmp == 0)
+		return (-1);
+	return (0);
+}
