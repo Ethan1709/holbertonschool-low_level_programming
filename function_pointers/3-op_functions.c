c585cdd7fca6817992c3be40c4b62abce494b9e0
Ethan1709 <benyayerethan@gmail.com>
2022-11-03 07:42:35 -0700

    task 3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -4,7 +4,7 @@
  * op_add - addition
  * @a: int
  * @b: int
- * Return : the operation
+ * Return: the operation
  */
 
 int op_add(int a, int b)
@@ -16,7 +16,7 @@ int op_add(int a, int b)
  * op_sub - difference
  * @a: int
  * @b: int
- * Return : the operation
+ * Return: the operation
  */
 
 int op_sub(int a, int b)
@@ -28,7 +28,7 @@ int op_sub(int a, int b)
  * op_mul - multiplication
  * @a: int
  * @b: int
- * Return : the operation
+ * Return: the operation
  */
 
 int op_mul(int a, int b)
@@ -40,7 +40,7 @@ int op_mul(int a, int b)
  * op_div - division
  * @a: int
  * @b: int
- * Return : the operation
+ * Return: the operation
  */
 
 int op_div(int a, int b)
@@ -52,7 +52,7 @@ int op_div(int a, int b)
  * op_mod - modulo
  * @a: int
  * @b: int
- * Return : the operation
+ * Return: the operation
  */
 
 int op_mod(int a, int b)
