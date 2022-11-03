45054a7a14f888d7a87f98c56743e49ce7006d78
Ethan1709 <benyayerethan@gmail.com>
2022-11-03 13:54:53 -0700

    task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers
+ * @separator: char pointer
+ * @n: number of parameters
+ * Return: Success
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+{
+	unsigned int i;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		printf("%d\n(separator)", i);
+	va_end(ap);
+}
