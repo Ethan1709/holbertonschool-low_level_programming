4656f55f4443d1d97009ed4c47296af15597d363
Ethan1709 <benyayerethan@gmail.com>
2022-11-03 15:20:56 -0700

    task1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,6 +19,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		j = va_arg(ap, unsigned int);
 		printf("%d%s", j, separator);
+	}
+	{
 		_putchar('\n');
 	}
 	va_end(ap);
