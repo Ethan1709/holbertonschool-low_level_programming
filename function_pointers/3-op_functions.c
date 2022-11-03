e0cbb05a3631a5a137e8385bc46e5e7f914c390c
Ethan1709 <benyayerethan@gmail.com>
2022-11-03 07:16:13 -0700

    task

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@
  * Return : the operation
  */
 
-int op_add(int a, int b);
+int op_add(int a, int b)
 {
 	return (a + b);
 }
@@ -19,7 +19,7 @@ int op_add(int a, int b);
  * Return : the operation
  */
 
-int op_sub(int a, int b);
+int op_sub(int a, int b)
 {
 	return (a - b);
 }
@@ -31,7 +31,7 @@ int op_sub(int a, int b);
  * Return : the operation
  */
 
-int op_mul(int a, int b);
+int op_mul(int a, int b)
 {
 	return (a * b);
 }
@@ -43,7 +43,7 @@ int op_mul(int a, int b);
  * Return : the operation
  */
 
-int op_div(int a, int b);
+int op_div(int a, int b)
 {
 	return (a / b);
 }
@@ -55,7 +55,7 @@ int op_div(int a, int b);
  * Return : the operation
  */
 
-int op_mod(int a, int b);
+int op_mod(int a, int b)
 {
 	return (a % b);
 }
