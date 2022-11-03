7154e654b6127515e309a7c67a505c3fabaa9f5c
Ethan1709 <benyayerethan@gmail.com>
2022-11-03 12:14:08 -0700

    task 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum of the arguments
+ * @n: int
+ * Return: Success
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+
+	va_start(ap, n);
+	
+	if (n == 0)
+		return (0);
+
+	while (n > 0)
+	{
+		int i;
+
+		i = va_arg(ap, int);
+		--n;
+	}
+	va_end(ap);
+}
+
+		
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _VARIADIC_H
+#define _VARIADIC_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+#endif
