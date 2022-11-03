86708d7c422e2bd56caaf4c3d1fce70f98b627eb
Ethan1709 <benyayerethan@gmail.com>
2022-11-03 13:04:56 -0700

    task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,13 +9,16 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
+	const unsigned int i, sum;
 
 	va_start(ap, n);
 
 	if (n == 0)
 		return (0);
-       	va_arg(ap, int);
 
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	return (sum);
 	va_end(ap);
-	return (0);
-}	
+}
