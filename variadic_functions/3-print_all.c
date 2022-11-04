7085a72f18d91348a8546fb198ee0c37d6dc2462
Ethan1709 <benyayerethan@gmail.com>
2022-11-04 05:55:24 -0700

    task 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print numbers
+ * @separator: char pointer
+ * @n: number of parameters
+ * Return: Success
+ */
+
+void print_all(const char * const format, ...)
+{
+	typedef struct unit {
+		char c;
+		int i;
+		float f;
+		char *s;
+	} unit;
+
+	int y = 0;
+
+	va_list ap;
+
+	va_start(ap, format);
+
+	while (format[y])
+		unit.[y] = var_arg(ap, char *);
+		{
+			printf("%s", unit.[y ]);
+			y++;
+		}
+	printf("\n");
+	va_end(ap);
+}
