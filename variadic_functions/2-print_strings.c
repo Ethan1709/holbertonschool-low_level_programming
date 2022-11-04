78fab22c0714e8305aa94bc5049027762c6b86ac
Ethan1709 <benyayerethan@gmail.com>
2022-11-04 03:24:53 -0700

    task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		j = va_arg(ap, char *);
-		printf("%s", j);
+		printf("%d", j);
 		if (i == n - 1)
 			continue;
 		if (separator != NULL)
