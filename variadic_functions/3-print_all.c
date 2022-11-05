15e3605933524269bc5c35ba645ecf233829cd2c
Ethan1709 <benyayerethan@gmail.com>
2022-11-05 10:49:27 -0700

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,33 +2,24 @@
 
 /**
  * print_all - print numbers
- * @separator: char pointer
- * @n: number of parameters
+ * @format: char pointer
  * Return: Success
  */
 
 void print_all(const char * const format, ...)
 {
-/**	struct chong[] = {
-		{"c", pchar},
-		{"i", pint},
-		{"f", pfloat},
-		{"s", pstring},
-		{NULL, NULL}
-	}; */
-
 	char *i = 0;
+	char *j = 0;
 
 	va_list ap;
 
 	va_start(ap, format);
 
-	while (i < format)
+	while (i < format - 1)
 	{
-	       i++;	
-		va_arg(ap, char *);
+		i++;
+		j = va_arg(ap, char *);
 	}
 	printf("\n");
 	va_end(ap);
 }
-		
