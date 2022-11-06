1249f05418fa2baaad24f0943ce38d6ac265e0e3
Ethan1709 <benyayerethan@gmail.com>
2022-11-06 07:25:18 -0800

    task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -11,4 +11,16 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct print - def of struct
+ * @c: char
+ * @f: function
+ */
+
+typedef struct print
+{
+	char *c;
+	void (*f)(va_list args);
+} print_t;
 #endif
