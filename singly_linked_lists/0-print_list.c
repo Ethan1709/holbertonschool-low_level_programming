995dbd5369efa79968991e3b29d100ab216e5ace
Ethan1709 <benyayerethan@gmail.com>
2022-11-14 13:53:21 -0800

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,14 +10,14 @@ size_t print_list(const list_t *h)
 {
 	size_t node;
 
-	while (h)
+	while (h != NULL)
 	{	
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-	node++;
-	h = h->next;
+		node++;
+		h = h->next;
 	}
 	return (node);
 }
