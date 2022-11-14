bcb1cbb4db6bee1228389f312e7b15bee6b90e28
Ethan1709 <benyayerethan@gmail.com>
2022-11-14 13:47:57 -0800

    task 0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,14 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _putchar(char c);
-size_t print_list(const list_t *h);
-
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
 
 #endif
