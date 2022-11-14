b0ecd6531652e95feeb4fa69373a6b92ff3f667e
Ethan1709 <benyayerethan@gmail.com>
2022-11-14 07:20:44 -0800

    task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements
+ * @h: pointer
+ * Return: Success
+ */
+
+size_t print_list(const list_t *h)
+{
+	if (str == NULL)
+		printf("[%d]" "(%s)", 0, nil);
+
+	return (0);
+}
---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,7 @@
+#ifndef _SINGLY_LINKED_LISTS
+#define _SINGLY_LINKED_LISTS	
+#include <stdio.h>
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+#endif
