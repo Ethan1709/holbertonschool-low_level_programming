c744e36757702197a3226086460b0d7f3cc09700
Ethan1709 <benyayerethan@gmail.com>
2022-11-14 13:42:16 -0800

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 /**
  * print_list - prints all the elements
  * @h: pointer on list_t
- * Return: Success
+ * Return: node
  */
 
 size_t print_list(const list_t *h)
@@ -11,10 +11,15 @@ size_t print_list(const list_t *h)
 	size_t node;
 
 	while (h != NULL)
+	{	
 		if (h->str == NULL)
 			printf("[0] (nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 	node++;
 	h->next;
+	}
+	return (node);
 }
+
+
