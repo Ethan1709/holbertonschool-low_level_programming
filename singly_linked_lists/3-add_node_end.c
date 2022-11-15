33e8e016c8bc3cee7f6ad7535409cf3f1f39faa4
Ethan1709 <benyayerethan@gmail.com>
2022-11-15 06:28:23 -0800

    task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -37,7 +37,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = newnode;
 	else
 	{
-		*tmp = *head;
+		tmp = *head;
 		while (tmp->next != NULL)
 			tmp = tmp->next;
 		tmp->next = newnode;
