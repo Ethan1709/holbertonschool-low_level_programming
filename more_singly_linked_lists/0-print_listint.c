0225592dbf6a5ed14345719a6f3216e3b0c88e6d
Ethan1709 <benyayerethan@gmail.com>
2022-11-15 11:55:53 -0800

    task 0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -11,8 +11,13 @@ size_t print_listint(const listint_t *h)
 	int node = 0;
 
 	while (h)
+	{
+		if (h == NULL)
+			return (1);
+		else
+			printf("%d\n", h->n);
 		node++;
-	printf("%i", node);
-
+		h = h->next;
+	}
 	return (node);
 }
