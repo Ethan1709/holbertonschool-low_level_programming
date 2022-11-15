e5b3a945018a9c56ba7a97729dfddc77c883f272
Ethan1709 <benyayerethan@gmail.com>
2022-11-15 05:06:56 -0800

    task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -29,7 +29,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	newnode->str = dup;
 	newnode->len = i;
-	newnode->NULL = *head;
+	newnode->next = NULL;
 	*head = newnode;
 
 	return (newnode);
