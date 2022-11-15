5db1f9a642e8d5a1534a7b0d25f71867ead8a3eb
Ethan1709 <benyayerethan@gmail.com>
2022-11-15 03:09:35 -0800

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,10 +11,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	char *dup;
+	list_t *newnode;
 
 	dup = strdup(str);
-	list_t *newnode = malloc(sizeof(list_s));
-	newnode->data = dup;
+	list_t *newnode = malloc(sizeof(list_t));
+	
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->str = dup;
 	newnode->next = *head;
 	*head = newnode;
+
 }
