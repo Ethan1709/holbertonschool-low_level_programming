8e6a43cd31b3e658a21ea0d4a4ab6a0d092530f2
Ethan1709 <benyayerethan@gmail.com>
2022-11-15 10:45:43 -0800

    task 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elmeents of the list
+ * @h: pointer on the structure
+ * Return: node
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t node;
+
+	while (h)
+		node++;
+	printf("%d", h);
+
+	return (node);
+}
---
+++ more_singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef _MORE_LINKED
+#define _MORE_LINKED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+/**
+ * struct listint_s - data structure 
+ * @n: int
+ * @next: pointer on structure 
+ */
+
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+size_t print_listint(const listint_t *h);
+#endif
