24a6a0c8a2f03529912a951592b01495909ada5e
Ethan1709 <benyayerethan@gmail.com>
2022-11-16 01:36:05 -0800

    task 3

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,6 +21,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (*head == NULL)
 		*head = newnode;
-
+	else
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = next;
+		tmp->next = newnode;
+	}
 	return (newnode);
 }
