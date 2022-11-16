3f195834963cad527ee4603ad4db0deb0f2145d7
Ethan1709 <benyayerethan@gmail.com>
2022-11-16 14:24:25 -0800

    task 7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		if (len == index)
-			return (head->n);
+			return (head->len);
 		len++;
 		head = head->next;
 	}
