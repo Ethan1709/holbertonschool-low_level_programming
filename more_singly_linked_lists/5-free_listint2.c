93b7520323b74daa29d5251335069c3062ff9f94
Ethan1709 <benyayerethan@gmail.com>
2022-11-16 02:53:19 -0800

    task 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *newnode;
 
-	while ((*head) != NULL)
-	{
-		newnode = (*head);
-		(*head) = (*head)->next;
-		free(newnode);
-	}
-	if ((*head) == NULL)
+	while ((*head))
 	{
 		newnode = (*head);
 		(*head) = (*head)->next;
