10319cdc1d8d3caa807fcc99e4970ecefb000b67
Ethan1709 <benyayerethan@gmail.com>
2022-11-16 01:49:43 -0800

    task 4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees listint_t
+ * @head: double pointer on listint_t
+ * Return: node
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *newnode;
+
+	while (head != NULL)
+	{
+		newnode = head;
+		free(head->n);
+		head = head->next;
+		free(newnode);
+	}
+}
