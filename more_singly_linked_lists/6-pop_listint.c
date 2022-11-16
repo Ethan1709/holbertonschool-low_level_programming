97d6f0422570bcfbe3aa7824015e27135c2e6596
Ethan1709 <benyayerethan@gmail.com>
2022-11-16 12:08:18 -0800

    task 6

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - frees listint_t
+ * @head: double pointer on listint_t
+ * Return: head
+ */
+
+int pop_listint(listint_t **head)
+{
+	int tmp = 0;
+
+	if (head == NULL)
+		return (0);
+
+	tmp = head;
+	head = head->next;
+
+	return (head);
+}
