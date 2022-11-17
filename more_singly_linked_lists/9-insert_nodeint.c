9752f20ae214ffe523b53bb1e7a36809dcb89511
Ethan1709 <benyayerethan@gmail.com>
2022-11-17 04:16:09 -0800

    task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -12,30 +12,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newnode;
 	unsigned int j = 0;
-	listin_t *newnode2;
 
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 
-	if (n == 1)
-	{
-		newnode->next = *head;
-		*head = newnode;
-	}
-	for (j = 0; j < n - 2; j++)
-	{
-		newnode2 = newnode2->next;
-	}
-	newnode->next = newnode2->next;
-	newnode2->next = newnode;
-
-	return (newnode);
-
-/**	while (j != idx)
+	while (j != idx)
 		j++;
 	newnode->n = n;
 	*head = newnode;
 
-	return (newnode);*/
+	return (newnode);
 }
