8aac53e408e1bc94a4d6c941288affa73b571302
Ethan1709 <benyayerethan@gmail.com>
2022-11-17 04:18:14 -0800

    task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (j != idx)
 		j++;
 	newnode->n = n;
+	(*head) = (*head)->next;
 	*head = newnode;
 
-	return (newnode);
+	return (head->next);
 }
