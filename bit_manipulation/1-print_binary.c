0e739e42281ba785c402d1cbfde5a1e348f584f5
Ethan1709 <benyayerethan@gmail.com>
2022-11-18 05:34:45 -0800

    task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: insigned long int
+ * Return: bit representation
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int i;
+
+	for (int i = 7; i >= 0; i--)
+		cout << ((a >> i) & 1);
+}
