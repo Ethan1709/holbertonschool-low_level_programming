3d1898dfaea6700694a371c89aad530fa87fad50
Ethan1709 <benyayerethan@gmail.com>
2022-11-18 04:13:55 -0800

    task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	if (b[i] < 48  || b[i] > 49)
+	if (b[i] < 48  || b[i] > 57)
 		return (0);
 
 	while (b[i] == '0' || b[i] == '1')
