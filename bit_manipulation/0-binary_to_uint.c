4afb2160c1798092c20fa4d74f0115dad16ca448
Ethan1709 <benyayerethan@gmail.com>
2022-11-18 04:02:10 -0800

    task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,11 +13,11 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	if (b[i] != '0' || b[i] != '1')
-		return (0);
 
 	while (b[i] == '0' || b[i] == '1')
 	{
+		if (b[i] != '0' || b[i] != '1')
+			return (0);
 		dec <<= 1;
 		dec += b[i] - '0';
 		i++;
