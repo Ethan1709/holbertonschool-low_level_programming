26f11a9bcc0ffda6620e89ae793935b18966f3cd
Ethan1709 <benyayerethan@gmail.com>
2022-11-19 11:20:26 -0800

    task 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
+/**
+ * countbits - count the number of bits to be flipped
+ * @n: uli
+ * Return: the good number of bits
+ */
+
 unsigned int countbits(unsigned long int n)
- {
-        unsigned long int i = 0;
-        
+{
+	unsigned long int i = 0;
+	
 	while (n != 0)
 	{
-            i++;
-            n &= (n - 1);
-        }
-        return (i);
-    }
+		i++;
+		n &= (n - 1);
+	}
+	return (i);
+}
 /**
  * flip_bits - flip bits
  * @n: insigned long int
