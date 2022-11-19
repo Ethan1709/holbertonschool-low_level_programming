0c52de28729d434015647ec862311dd553773e6e
Ethan1709 <benyayerethan@gmail.com>
2022-11-19 10:23:29 -0800

    task 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip bits
+ * @n: insigned long int
+ * @m: uli
+ * Return: number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+{
+	while (n != 0);
+	{
+		m++;
+		n &= (n - 1);
+	}
+	return (m);
+}
