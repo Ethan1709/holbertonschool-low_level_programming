91997c5d473e5a41bde278a95389d00f8157819f
Ethan1709 <benyayerethan@gmail.com>
2022-11-19 11:49:15 -0800

    task 10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a node at given position
+ * @head: double pointer on listint_t
+ * @index: ui
+ * Return: newnode
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int j = 0;
+	listint_t *node;
+	listint_t *del = *head;
+
+	if (del == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		(*head) = (*head) -> next;
+		free(tmp);
+		return (1);
+	}
+	while (j < index - 1)
+	{
+		if (del == NULL)
+			return(-1);
+		node == *head;
+		(*head) == (*head) -> next;
+		free(node);
+	}
+}	
