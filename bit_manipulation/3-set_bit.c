adb146ecfb5bd222b35d562ea4ebeaca7a97227a
Ethan1709 <benyayerethan@gmail.com>
2022-11-19 09:54:15 -0800

    task 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - set valus of a bit to 1
+ * @n: insigned long int
+ * @index: ui
+ * Return: value of a bit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index);
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
