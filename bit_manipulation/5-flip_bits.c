545ea435a7cb0f68aa899826674ef529f6e67127
Ethan1709 <benyayerethan@gmail.com>
2022-11-19 11:10:53 -0800

    task 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,31 +1,20 @@
 #include "main.h"
 
 /**
- * countsetbits - recursive function for flipping bits
- * @n: int
- * Return: number of bits
- */
-
-unsigned int countsetbits(int n)
-{
-	int i = 0;
-
-	while (n > 0)
-	{
-		i++;
-		n &= (n - 1);
-	}
-	return (i);
-}
-
-/**
  * flip_bits - flip bits
  * @n: insigned long int
  * @m: uli
- * Return: number of bits
+ * Return: number of bits to be flipped
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return (countsetbits(n ^ m));
+	unsigned int bits = 0;
+
+	while ((n ^ m) > 0)
+	{
+		bits += ((n ^ m) & 1);
+		(n ^ m) >>= 1;
+	}
+	return (bits);
 }
