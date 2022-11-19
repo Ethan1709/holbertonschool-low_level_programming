4c379599dd68b968d644c12f0c70827ebe3f7c5f
Ethan1709 <benyayerethan@gmail.com>
2022-11-19 11:53:22 -0800

    task 10

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (del == NULL)
 			return(-1);
-		*node == (*head);
+		node = (*head);
 		(*head) = (*head) -> next;
 		free(node);
 	}
