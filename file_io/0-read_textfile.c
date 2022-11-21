98fa540b159db36aac1940a6635ee6919e3065e5
Ethan1709 <benyayerethan@gmail.com>
2022-11-21 06:13:25 -0800

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -16,9 +16,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buf == NULL)
 		return (0);
 
-	o = open(filename, O_RDONLY);
+	o = open(filename, O_RDONLY || );
 	r = read(o, buf, letters);
-	w = write(1, buf, r);
+	w = write(STDOUT_FILENO², buf, r);
 
 	if (o == -1 || r == -1 || w != r)
 	{
