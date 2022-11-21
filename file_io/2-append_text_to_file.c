2e3a0c46195f56344f49bf6d022b38e6b5ed4e6b
Ethan1709 <benyayerethan@gmail.com>
2022-11-21 13:55:51 -0800

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -21,8 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
-		w = write(o, text_content, i);
 	}
+	w = write(o, text_content, i);
 	if (w == -1)
 		return (-1);
 	close(o);
