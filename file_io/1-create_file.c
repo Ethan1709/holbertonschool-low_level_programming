b04f81fa5a6a6e6e302e8d4f09a8494e1fecccc9
Ethan1709 <benyayerethan@gmail.com>
2022-11-21 07:34:35 -0800

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	
 	if (o == -1)
 		return (-1);
-	for (i = 0; i < text_content; i++)
+	for (i = 0; text_content[i] != '\0'; i++)
 		;
 	if (text_content != NULL)
 	{
