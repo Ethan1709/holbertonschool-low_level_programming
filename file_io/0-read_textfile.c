82d366c853c4bd3dba421975b746ab226f5f8fb3
Ethan1709 <benyayerethan@gmail.com>
2022-11-21 03:01:21 -0800

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,10 +9,16 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int i = 0;
+
 	if (!filename)
 		return (0);
 	if (!letters)
 		return (0);
+
+	while (i < filename)
+		_putchar(filename);
+
 	return (letters);
 }
 
