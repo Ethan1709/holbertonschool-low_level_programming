1adfd4e75f463f11040c4d329314cb2e73d69247
Ethan1709 <benyayerethan@gmail.com>
2022-11-21 05:01:36 -0800

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,8 +9,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t fd;
+	char *buf;
+
 	if (!filename || !letters)
 		return (0);
-	return (0);
+
+	buf = malloc(sizeof(char) * letters);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	read(fd, buf, letters);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	write(1, buf letters);
+	free(buf);
+	return (letters);
 }
 
