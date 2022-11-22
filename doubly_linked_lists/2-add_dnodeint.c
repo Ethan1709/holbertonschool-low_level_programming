f09119bc788e61c1c86add9f29335b5579b434c1
Ethan1709 <benyayerethan@gmail.com>
2022-11-22 05:26:44 -0800

    task 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of the list
+ * @h: double ptr
+ * @n: int
+ * Return: return number of the elements
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
