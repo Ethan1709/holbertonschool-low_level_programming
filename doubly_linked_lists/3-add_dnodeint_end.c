a1b4ed43f1c47c5c0d6fbf7935437942e503fa89
Ethan1709 <benyayerethan@gmail.com>
2022-11-22 14:42:07 -0800

    task 3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,8 +15,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->prev = (*head)->next;
+	new->prev = *head;
 	new->next = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
 	if (*head == NULL)
 		*head = new;
 	else
