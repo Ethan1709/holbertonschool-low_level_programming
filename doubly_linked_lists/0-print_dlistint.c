1308d3540d2ae6afd24674edd6c2ad34ddd71980
Ethan1709 <benyayerethan@gmail.com>
2022-11-22 02:06:03 -0800

    task 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,10 +11,10 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t node = 0;
 
 	while (h != NULL)
-	{
+	{	
+		h = h->previous;
 		node++;
 		h = h->next;
-		h = h->prev;
 	}
 	return (node);
 }
