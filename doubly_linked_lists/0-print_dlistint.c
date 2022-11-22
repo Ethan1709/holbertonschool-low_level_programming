1dfe1d41ed36fe49307501299b98e48eb06bf7ea
Ethan1709 <benyayerethan@gmail.com>
2022-11-22 01:44:51 -0800

    task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_distint - prints all the elements 
+ * @h: ptr dlistint_t
+ * Return: return all the elements 
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,8 @@
+#ifndef _DOUBLY_LINKED
+#define _DOUBLY_LINKED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t print_dlistint(const dlistint_t *h);
+#endif
