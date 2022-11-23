499ea2725796fe6f66d40017aa4c063092305a1e
Ethan1709 <benyayerethan@gmail.com>
2022-11-23 04:19:38 -0800

    task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	new = tmp->next;
 	if (tmp != NULL)
-	{	
+	{
 		new->prev = tmp;
 		new->next = tmp->next;
 		(tmp->next)->prev = new;
