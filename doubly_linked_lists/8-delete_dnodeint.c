a53a2c4a1cee6df9eb8aac47f412518dc9f41961
Ethan1709 <benyayerethan@gmail.com>
2022-11-23 13:12:00 -0800

    task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,16 +2,15 @@
 
 /**
  * delete_dnodeint_at_index - function that deletes a node
- * @h: doyble ptr
- * @idx: ui
- * @n: int
+ * @head: double ptr
+ * @index: ui
  * Return: return the node
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t *tmp ;
+	dlistint_t *tmp;
 	dlistint_t *del = *head;
 
 	if (del == NULL)
@@ -23,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(del);
 		return (1);
 	}
-	
+
 	while (i != index - 1)
 	{
 		if (del->next == NULL)
