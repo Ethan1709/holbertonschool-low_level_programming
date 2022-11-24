dbd3d65bc30bc41f4576e4014d4e2be72e19a419
Ethan1709 <benyayerethan@gmail.com>
2022-11-24 08:55:39 -0800

    task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	htable->size = size;
 
 	for (i = 0; i < htable->size; i++)
-		htable->hash_table_t[i] = NULL;
+		htable->key[i] = NULL;
 
 	return (htable);
 }
