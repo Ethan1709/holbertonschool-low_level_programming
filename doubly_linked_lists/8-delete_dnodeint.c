5fc933e2a9d780f612dc3fc1dc713bbb39e32699
Ethan1709 <benyayerethan@gmail.com>
2022-11-23 16:18:25 -0800

    task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,7 +11,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
 
-	if (tmp == NULL)
+	if (*head == NULL)
 		return (-1);
 
 	for (; index != 0; index--)
