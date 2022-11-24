9e535956033508ff9e1992790262276552e58455
Ethan1709 <benyayerethan@gmail.com>
2022-11-23 16:05:29 -0800

    task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -29,11 +29,20 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		i++;
 		tmp = tmp->next;
 	}
+	
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
 
-	tmp->prev->next = tmp->next;
-	if (tmp->next != NULL)
-		tmp->next->prev = tmp->prev;
-	free(tmp);
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+		free(tmp);
 
 	return (1);
 }
