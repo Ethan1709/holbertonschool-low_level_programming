9ed810577a571162bb61b7f02f2432fc9cd745a5
Ethan1709 <benyayerethan@gmail.com>
2022-11-24 09:15:34 -0800

    task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,11 +11,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *htable;
 
-	htable->array = malloc(sizeof(hash_node_t) * size);
+	htable = malloc(sizeof(hash_node_t) * size);
 	if (htable == NULL)
 		return (NULL);
 
-	htable->size = size;
+	htable->array = size;
 
 	for (i = 0; i < htable->size; i++)
 		htable->array[i] = NULL;
