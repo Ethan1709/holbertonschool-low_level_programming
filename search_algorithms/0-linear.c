8d3de3b86d7fc401c716b99c261ea3f666384f06
Ethan1709 <benyayerethan@gmail.com>
2023-01-03 02:07:54 -0800

    first task

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of ints
+ * @array: array to search in
+ * @size: number of elements in the array
+ * @value: value to search
+ *
+ * Return: the value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (value);
+		else
+			return (-1);
+	}
+}
+
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_ALGO
+#define _SEARCH_ALGO
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
