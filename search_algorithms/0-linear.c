7045bd77024be046591eaf28051cd1cf21d07e36
Ethan1709 <benyayerethan@gmail.com>
2023-01-03 02:56:35 -0800

    fixing 0-linear file

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] != value)
-			printf("Value checked array[%d] = [%d]\n", i , array[i]);
+			printf("Value checked array[%i] = [%d]\n", i , array[i]);
 		else
 			return (array[i]);
 	}
